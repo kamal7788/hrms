7434a5cafddc1551603e7ebc209ca0849f534b54
Asmita Hase <asmitahase@gmail.com>
2026-04-23 10:32:41 +0530

    fix(patch): check if sidebar table exists before checking if sidebar exists
    (cherry picked from commit 5181b843c9c6431022ea96dc99628e65589d57d2)

--- hrms/patches/v16_0/make_people_workspace_sidebar_standard.py
+++ hrms/patches/v16_0/make_people_workspace_sidebar_standard.py
@@ -2,5 +2,6 @@ import frappe
 
 
 def execute():
-	if frappe.db.exists("Workspace Sidebar", "People"):
-		frappe.db.set_value("Workspace Sidebar", "People", "standard", 1)
+	if frappe.db.table_exists("Workspace Sidebar"):
+		if frappe.db.exists("Workspace Sidebar", "People"):
+			frappe.db.set_value("Workspace Sidebar", "People", "standard", 1)
